f00aec611cb6c3b528fe2656ddb3857a45a05589
Mahesh Kshirsagar <maksh@microsoft.com>
2020-09-02 18:12:30 +0100

    added udr setting

---
+++ Infra/deployscript.ps1
@@ -0,0 +1,19 @@
+$rgName="vwanaks-rg"
+$location="uksouth"
+$APP_ID=$(az keyvault secret show --name "clientid" --vault-name "maksh-key-vault" --query value)
+$APP_SECRET=$(az keyvault secret show --name "clientsecret" --vault-name "maksh-key-vault" --query value)
+$TENANT_ID=$(az keyvault secret show --name "tenantid" --vault-name "maksh-key-vault" --query value)
+
+# Login
+az login --service-principal --username $APP_ID --password $APP_SECRET --tenant $TENANT_ID
+
+# Create Resource Group
+az group create -n $rgName  -l $location
+
+# Enable Key Vault for ARM Deployment access (Only 1 time)
+az keyvault update --enabled-for-template-deployment true -n maksh-key-vault
+
+# Validate ARM
+az deployment group validate -g $rgName `
+    --template-file azuredeploy.json `
+    --parameters azuredeploy.parameters.json
